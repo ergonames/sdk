5745de179f93e5e5f1204a84b75e6a17cbfdddc6
Zack Balbin <zack@zackbalbin.com>
2022-09-11 11:20:30 -0500

    change from_testnet_str to from_mainnet_str

--- tx-lib/index.js
+++ tx-lib/index.js
@@ -62,7 +62,7 @@ export async function send_transaction(ergoname_price, ergoname_name, reciever_a
 
             let ergoname_name_bytes = Uint8Array.from(Buffer.from(ergoname_name, 'utf8'));
 
-            let reciever_address_type = wasm.Address.from_testnet_str(reciever_address);
+            let reciever_address_type = wasm.Address.from_mainnet_str(reciever_address);
             let reciever_address_ergotree = reciever_address_type.to_ergo_tree();
             let reciever_address_bytes = reciever_address_ergotree.sigma_serialize_bytes();
             
