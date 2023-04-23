a1fe1207f9944e1400467df1f2ee88d6d7756488
Zack Balbin <zack@zackbalbin.com>
2023-04-23 10:14:45 -0500

    new proxy

--- tx-lib/index.js
+++ tx-lib/index.js
@@ -1,23 +1,22 @@
-import { ErgoAddress, OutputBuilder, SColl, SByte, SConstant, SLong, TransactionBuilder } from "@fleet-sdk/core";
+import { ErgoAddress, OutputBuilder, SColl, SByte, SConstant, SLong, SSigmaProp, TransactionBuilder } from "@fleet-sdk/core";
 
 const DEFAULT_EXPLORER_URL = "https://api-testnet.ergoplatform.com";
-const ERGONAMES_CONTRACT_ADDRESS = "2QSobRecPvrMVpdNVdqiEcDjCoZYyMCXRwh8cA8M5qx3rwiuQA5bifAzghk";
+const ERGONAMES_PROXY_ADDRESS = "34sQ9WYJ67tfGss9Za6jpiM6s1F9QAV2GR2Cvc3N5zJFVTuqTo68R9tBktFuwTZ7C67QX8xutEsV7Rsn8wUZno67EcRAf9hJ8wKZr6NDseFK6J3aCk76i3VWD3uqrnwqkQX1hEK8MVu1uXhoizUuKkuNQtKwYeMUK6yG3B3id4SoeQzgx24pqJEgw6cTjrNirjib7ossgbk3WDMCGpQt6htYDZWcmBPALxaxjrZWBRBsdNeVWFZckZ";
 
-export async function sendTransaction(name, receiverAddress, explorerUrl = DEFAULT_EXPLORER_URL) {
+export async function sendTransaction(name, ergonamePrice, receiverAddress, explorerUrl = DEFAULT_EXPLORER_URL) {
     let currentHeight = await getCurrentHeight(explorerUrl);
-    let amountToSend = 1000000 + (1000000 * 2);
+    let amountToSend = ergonamePrice + (1000000 * 2);
     let inputs = await ergo.get_utxos(amountToSend);
 
     let receiverErgoAddress = ErgoAddress.fromBase58(String(receiverAddress));
     let receiverErgoTree = receiverErgoAddress.ergoTree;
-    receiverErgoTree = "0e24" + receiverErgoTree;
 
     const unsignedTransaction = new TransactionBuilder(currentHeight)
         .from(inputs)
-        .to(new OutputBuilder(amountToSend, ERGONAMES_CONTRACT_ADDRESS)
+        .to(new OutputBuilder(amountToSend, ERGONAMES_PROXY_ADDRESS)
             .setAdditionalRegisters({
                 R4: SConstant(SColl(SByte, Buffer.from(name, "utf-8"))).toString("hex"),
-                R6: receiverErgoTree,
+                R5: receiverErgoTree,
             })
         )
         .sendChangeTo(receiverAddress)
