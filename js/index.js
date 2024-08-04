6bd44fc10377fa55c33e79a5a6d0c99553239144
Zack Balbin <zack@zackbalbin.com>
2024-08-03 19:56:59 -0500

    mock js sdk

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
-import axios from 'axios';
-
-const BASE_API_URL = "https://ergonames-api.zackbalbin.com";
-
-export const getApiInfo = async () => {
-    const res = await axios.get(`${BASE_API_URL}/info`);
-    return res.data;
-};
-
-export const getErgoNameRegistrationData = async (name) => {
-    const res = await axios.get(`${BASE_API_URL}/resolve/${name}`);
-    return res.data;
+export function resolve(name) {
+    if (name === "balb") {
+        return {
+            ergoname: {
+                name: "balb",
+                token_id: "0fdb7ff8b37479b6eb7aab38d45af2cfeefabbefdc7eebc0348d25dd65bc2c91"
+            },
+            address: "9fp5SckQQkw11cMf4evhmjr81vJGTer2uUzE3F4SD3LAE2gKBCs"
+        }
+    }
+    return null;
 }
